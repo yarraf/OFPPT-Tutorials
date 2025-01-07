648027202f28205e73c7f7e80a1b06386ae55345
Youness Arraf <youness.arraf@inetum.com>
2025-01-07 10:43:15 +0100

    initialize apps manageusers with react-redux using createStore)

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+    users: [],
+    loading: false,
+    error: null,
+  };
+  
+  const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'FETCH_USERS_REQUEST':
+        return { ...state, loading: true, error: null };
+      case 'FETCH_USERS_SUCCESS':
+        return { ...state, loading: false, users: action.payload };
+      case 'FETCH_USERS_FAILURE':
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
+  
